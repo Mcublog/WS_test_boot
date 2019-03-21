0abfd5a6fda5a9efdd309dd9c200e956c101a928
Vyacheslav Konovalov <konovalov@healbe.com>
2019-03-21 14:26:45 +0300

    modify bit_to_array.py;

--- MDK-ARM/Utils/bin_to_array.py
+++ MDK-ARM/Utils/bin_to_array.py
@@ -7,8 +7,8 @@ in_file = 'WS_test_task.bin'
 # Create byte arry
 f = open(in_file, 'rb')
 array = bytearray(f.read())
-print(array)
 array_len = str(len(array))
+
 # Create .h
 f = open(out_file_name + '.h', 'w')
 f.write('#ifndef FIRMWARE_H\n')
@@ -17,6 +17,7 @@ f.write('#include <stdint.h>\n\n')
 
 f.write('#define FIRMWARE_SIZE (' + array_len + ')\n\n')
 
+# Byte in string
 sym_in_str = 32
 
 f.write('static const uint8_t firm_array[' + array_len + '] __attribute__((section (".ARM.__at_0x08004000"))) = {')
